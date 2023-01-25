f85cab48fe31adfeadc89d6024ab2c5912731ae8
souwang324 <33944965+souwang324@users.noreply.github.com>
2023-01-25 20:26:59 +0900

    Initial commit

---
+++ DICOMReaderTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kitware.VTK;
+
+namespace DICOMReaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DICOMReaderTest1();
+        }
+
+        public static void DICOMReaderTest1()
+        {
+            vtkDICOMImageReader dcmReader = vtkDICOMImageReader.New();
+            dcmReader.SetFileName("../../../../res/CT/CT.00002.00002.dcm");
+            dcmReader.Update();
+
+
+            vtkImageViewer2 imageViewer = vtkImageViewer2.New();
+            imageViewer.SetInputConnection(dcmReader.GetOutputPort());
+
+            vtkRenderWindow renderWin = vtkRenderWindow.New();
+            imageViewer.SetRenderWindow(renderWin);
+
+            renderWin.AddRenderer(imageViewer.GetRenderer());
+
+            vtkRenderWindowInteractor interactor = vtkRenderWindowInteractor.New();
+            interactor.SetRenderWindow(renderWin);
+
+            renderWin.Render();
+            interactor.Start();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DICOMReaderTest
+ DICOMReaderTest with CSharp Activiz x64 , Visual Studio 2017 Community, Windows10
